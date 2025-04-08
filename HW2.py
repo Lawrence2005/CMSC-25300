05bb91cd77ab2af79cf66543eec46fd7b0a7bb6c
Lawrence2005 <lawrenceee0126@gmail.com>
2025-04-07 21:08:47 -0500

    Done?

--- HW2.py
+++ HW2.py
@@ -3,12 +3,11 @@ import numpy as np
 # #### Part a #####
 # Load in training data and labels
 # File available on Canvas
-face_data_dict = np. load ("face_emotion_data.npz ")
+face_data_dict = np. load ("face_emotion_data.npz")
 face_features = face_data_dict ["X"]
 face_labels = face_data_dict ["y"]
 n, p = face_features.shape
 # Solve the least - squares solution . weights is the array of weight coefficients
 
-# TODO : find weights
 weights = np.dot((face_features.transpose() @ face_features).inv(), face_features.transpose() @ face_labels)
 print ( f" Part 4a. Found weights :\n{ weights }")
