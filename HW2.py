c9bf12a0bdbe4732194f3abec0cda904ed88092e
Lawrence2005 <lawrenceee0126@gmail.com>
2025-04-07 21:09:27 -0500

    Done?

--- HW2.py
+++ HW2.py
@@ -9,5 +9,5 @@ face_labels = face_data_dict ["y"]
 n, p = face_features.shape
 # Solve the least - squares solution . weights is the array of weight coefficients
 
-weights = np.dot((face_features.transpose() @ face_features).inv(), face_features.transpose() @ face_labels)
+weights = (face_features.transpose() @ face_features).inv() @ face_features.transpose() @ face_labels
 print ( f" Part 4a. Found weights :\n{ weights }")
